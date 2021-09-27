bbee09b40ca02d8bd4484f20a588b0abcae5e10b
Kristian Fulkerson <kristianf451@gmail.com>
2021-09-27 12:16:22 -0700

    ya, mvp

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,68 +1,74 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 
-// export default function TodoForm({ addTodo }) {
-//   const [text, setText] = useState("");
+export default function TodoForm({ addTodo }) {
+  const [state, setState] = useState({
+    input: ""
+  });
 
-//   const handleChanges = e => {
-//     setText(e.target.value);
-//   };
-
-//   const handleAdd = () => {
-//     addTodo(text);
-//     setText("");
-//   };
-//   return (
-//     <form>
-//       <input
-//         onChange={handleChanges}
-//         value={text}
-//         type="text"
-//         name="todo"
-//         placeholder="Enter A Todo..."
-//       />
-//       <button onClick={handleAdd}>Add Todo</button>
-//     </form>
-//   );
-// }
-import React from "react";
-
-class TodoForm extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      input: ""
-    };
-  }
-
-  //function to handle changes in the input field
-  handleChanges = e => {
-    //update state with each keystroke
-    this.setState({
+  const handleChanges = e => {
+    setState({
       input: e.target.value
     });
   };
 
-  handleClick = e => {
+  const handleAddClick = e => {
     e.preventDefault();
-    this.props.addTodo(this.state.input);
-    this.setState({
-      input: ""
-    });
+    addTodo(state.input);
+    setState({ input: "" });
   };
-
-  render() {
-    return (
-      <form>
-        <input
-          type="text"
-          name="todo"
-          onChange={this.handleChanges}
-          value={this.state.input}
-        />
-        <button onClick={this.handleClick}>Add 'To-Do'</button>
-      </form>
-    );
-  }
+  return (
+    <form>
+      <input
+        onChange={handleChanges}
+        value={state.input}
+        type="text"
+        name="todo"
+        placeholder="Enter A Todo..."
+      />
+      <button onClick={handleAddClick}>Add Todo</button>
+    </form>
+  );
 }
 
-export default TodoForm;
+// import React from "react";
+
+// class TodoForm extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       input: ""
+//     };
+//   }
+
+//   //function to handle changes in the input field
+//   handleChanges = e => {
+//     //update state with each keystroke
+//     this.setState({
+//       input: e.target.value
+//     });
+//   };
+
+//   handleClick = e => {
+//     e.preventDefault();
+//     this.props.addTodo(this.state.input);
+//     this.setState({
+//       input: ""
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <form>
+//         <input
+//           type="text"
+//           name="todo"
+//           onChange={this.handleChanges}
+//           value={this.state.input}
+//         />
+//         <button onClick={this.handleClick}>Add 'To-Do'</button>
+//       </form>
+//     );
+//   }
+// }
+
+// export default TodoForm;
